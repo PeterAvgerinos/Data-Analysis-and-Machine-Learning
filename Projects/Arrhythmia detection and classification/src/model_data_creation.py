ba1992d9d5ea5f0aa4d080ae8a33df32772d8ba4
ntua-el15074 <el15074@mail.ntua.gr>
2023-11-23 14:31:43 +0200

    data augmentation, gaussian noise

--- Projects/Arrhythmia detection and classification/src/model_data_creation.py
+++ Projects/Arrhythmia detection and classification/src/model_data_creation.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
 
 import pandas as pd
+import numpy as np
 from glob import glob
 from sklearn.model_selection import train_test_split
+
+def add_noise(df, noise_level=0.1):
+    data_range = df.max() - df.min()
+
+    noise = np.random.normal(scale=noise_level*data_range, size=df.shape)
+    return df + noise
 
 paths = glob('../csv_files/MLII/MLII_*.csv')
 dfs = []
@@ -12,6 +19,25 @@ for path in paths:
     dfs.append(df)
 
 combined_df = pd.concat(dfs, ignore_index=True)
+
+features = combined_df.iloc[:, :-1]
+labels = combined_df.iloc[:, -1]
+
+augmented_features = []
+augmented_labels = []
+
+for index, row in features.iterrows():
+    noisy_feature = add_noise(row)
+    augmented_features.append(noisy_feature.values)
+    augmented_labels.append(labels[index])
+
+augmented_features = pd.DataFrame(augmented_features, columns=features.columns)
+augmented_labels = pd.Series(augmented_labels, name=labels.name)
+
+augmented_data = pd.concat([augmented_features, augmented_labels], axis=1)
+
+combined_df = pd.concat([augmented_data, combined_df], ignore_index=True)
+combined_df = combined_df.sample(frac=1).reset_index(drop=True)
 
 train_val, test = train_test_split(combined_df, test_size=0.15, random_state=42)
 train, validate = train_test_split(train_val, test_size=0.15, random_state=42)
